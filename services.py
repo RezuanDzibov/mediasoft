26ddad5cb162c32af08841875397357d9d6ccb40
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-02-07 21:50:56 +0300

    Add get_city_streets service

--- services.py
+++ services.py
@@ -1,14 +1,26 @@
+from uuid import UUID
+from typing import List
+
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Load
 
-from db.models import City
+from db.models import City, Street
 
 
-async def get_cities(session: AsyncSession):
+async def get_cities(session: AsyncSession) -> List[City]:
     statement = select(City).options(
         Load(City).load_only(City.id, City.name),
     )
     result = await session.execute(statement)
     cities = result.scalars().all()
     return cities
+
+
+async def get_city_streets(session: AsyncSession, city_id: UUID) -> List[Street]:
+    statement = select(Street).where(Street.city_id == city_id).options(
+        Load(Street).load_only(Street().id, Street.name),
+    )
+    result = await session.execute(statement)
+    streets = result.scalars().all()
+    return streets
